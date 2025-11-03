f3c35deac4341a4b8918124367395c164737d54f
Benjamen Bielecki <45702229+bcbielecki@users.noreply.github.com>
2025-11-02 19:06:58 -0800

    remove urllib.parse

--- src/abackup/jobs.py
+++ src/abackup/jobs.py
@@ -1,6 +1,5 @@
 from enum import Enum
 from pathlib import Path
-from urllib.parse import urlparse
 from datetime import time
 
 # Job class
--- src/abackup/schedule.py
+++ src/abackup/schedule.py
@@ -2,3 +2,6 @@
 
 # Schedule class
 
+# Holds a job pool and hands jobs to core for execution when
+# their scheduled time arrives
+
