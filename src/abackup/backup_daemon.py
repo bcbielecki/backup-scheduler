3fde3122df66507792fce86b9a96ec0a07a09cc8
Benjamen Bielecki <45702229+bcbielecki@users.noreply.github.com>
2025-11-03 20:46:46 -0800

    Fix missing "pass" statements in backup_daemon.py

--- src/abackup/backup_daemon.py
+++ src/abackup/backup_daemon.py
@@ -3,9 +3,11 @@ import platform
 
 if platform.system() == "Windows":
     #Do Windows-specific imports here
+    pass
 if platform.system() in ["Linux", "Darwin"]:
     #Do Unix-specific imports here
-    
+    pass
+
 # There should only ever be **one** Backup Daemon instance running per system.
 # This daemon will be responsible for starting/stopping the scheduler, executing the jobs,
 # and handling any system-level interactions required for background operation.
